a91498aeff8765fc71d7cc2f8f77a3faa591417a
Aiman Kayad <Akaya005@fiu.edu>
2022-08-17 13:58:14 -0400

    Update Main.java
    Fixed solution description

--- Main.java
+++ Main.java
@@ -29,9 +29,11 @@ You can return the answer in any order.
     /**
       SOLUTION: 
     - Initialze a new empty HashMap<Integer, Integer>
-    - Create a for loop and iterate over the length of the integer array, adding each value to your HashMap
-    - If an element in the integer array already exists in your Map, check if the complement to that number for target also exists in the Map. If it does, return an array of the key of the complement, and the index at that point in the loop. 
-    - Else, if it doesn't add that value to the HashMap
+    - Create a for loop and iterate over the length of the integer array
+    - Create an int variable named complement which is set up to calculate the difference between target and nums[i]
+    - Create an if statement that, if your hashmap contains a complement to your target (This value will be stored as a Key in your hmap<Key, Value> pair so specify that),
+    then return a new int[] array with two values of {the index of the complement stored in your hmap, i};
+    - Else, add the value from the nums array that you are currently on, into to your hmap.
     - If no solution found, at end of program, return an empty array.
     **/
     
